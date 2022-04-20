6e2f2ab14eb3453b916d44b002391ce8e4ca0c99
Gizem <gizem.candemir@secilstore.com>
2022-04-20 15:50:40 +0300

    signup and signin is creating

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 import express from "express";
-const port = 5000;
-
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./routes/user.js"
 
 const app= express();
-app.get("/", (req,res) =>{
-   res.send("hello express");
-})
 
-app.listen(port, () =>{
-    console.log(`Server lsitening on port ${port}`)
-})
+app.use(morgan("dev"));
+app.use(express.json({limit:"30mb", extended: true}));
+app.use(express.urlencoded({limit:"30mb", extended: true}));
+app.use(cors());
+
+app.use("/users", userRouter);
+
+const MONGODB_URL= "mongodb+srv://mhndsbgyn:452452@cluster0.khrqv.mongodb.net/sweet_db?retryWrites=true&w=majority";
+
+const port = 5000;
+
+mongoose.connect(MONGODB_URL).then(() => {
+
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    })
+}).catch((error) => console.log(`${error} did not connect`))
---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+
+import {signup} from "../controllers/user.js";
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+export default router;
